5788a89d4796903af6fa90b052d68a533a440811
Your GitHub Username <zunigam@mtmary.edu>
2026-04-20 04:13:43 -0500

    added custom css styling javascript interactivity and api placeholder setup

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-<script src="script.js"></script>
+// selecting buttons by thier id
+    let eventsBtn = document.getElementById("eventsBtn");
+    let diningBtn = document.getElementById("diningBtn");
+    let resourcesBtn = document.getElementById("resourcesBtn");
+
+// When users click on the event button
+if (eventsBtn) {
+    eventsBtn.addEventListener("click", function() {
+        alert("Going to Events Page!");
+    });
+}
+
+// When users click on the dining button
+if (diningBtn) {
+    diningBtn.addEventListener("click", function() {
+        alert("Going to Dining Page!");
+    });
+}
+
+// When users click on the resources button
+if (resourcesBtn) {
+    resourcesBtn.addEventListener("click", function() {
+        alert("Going to Resources Page!");
+    });
+}
+
+// api placeholder for future campus events
+let eventsApiUrl = "https://api.campus-events.com/events";
+
+// this function will be used later to get event data
+function getCampusEvents() {
+        console.log("API placeholder is set up");
+        console.log("future API URL:", eventsApiUrl);
+}
+
+// calling the function for now
+getCampusEvents();
